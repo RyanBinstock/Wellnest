eb1f66bd3a7956d2ad9f7a4ecca46167cf5eac0a
Ryan Binstock <ryanibinstock@gmail.com>
2025-11-09 20:06:16 -0500

    Add the Tasks table to the SnapTaskContract class

--- app/src/main/java/com/code/wlu/cp470/wellnest/data/local/contracts/SnapTaskContract.java
+++ app/src/main/java/com/code/wlu/cp470/wellnest/data/local/contracts/SnapTaskContract.java
@@ -27,5 +27,33 @@ public class SnapTaskContract {
     }
 
 
+    // =========================
+    //  tasks (stored user's tasks)
+    // =========================
+    public static final class Tasks {
+        private Tasks() {}
 
+        public static final String TABLE = "tasks";
+
+        public static final class Col {
+            public static final String UID = "uid";                  // TEXT PK
+            public static final String NAME =  "name";              // TEXT
+            public static final String POINTS = "points";            // INTEGER
+            public static final String DESCRIPTION = "description";  // TEXT
+            public static final String COMPLETED = "completed";      // BOOLEAN
+        }
+
+        // Default description column to empty string
+        // and completed column to FALSE
+        public static final String SQL_CREATE =
+                "CREATE TABLE " + TABLE + "(" +
+                        Col.UID + " TEXT PRIMARY KEY, " +
+                        Col.NAME + " TEXT NOT NULL, " +
+                        Col.POINTS + " INTEGER NOT NULL, " +
+                        Col.DESCRIPTION + " TEXT DEFAULT '', " +
+                        Col.COMPLETED + " BOOLEAN NOT NULL DEFAULT 0" + //0 -> FALSE, 1 -> TRUE
+                        ")";
+
+        public static final String SQL_INDEXES = ""; // none needed for the moments
+    }
 }
